f6eb306b994003d247f19ec533103d7b42a351cc
Vinnicius Toth <vinni.toth@gmail.com>
2026-02-21 16:55:33 -0300

    infra: Adicionando Security Group terraform

---
+++ infra/modules/security/main.tf
@@ -0,0 +1,30 @@
+resource "aws_security_group" "this" {
+  name        = var.sg_name
+  description = "Security group for FastAPI API"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow FastAPI"
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = var.tags
+}
