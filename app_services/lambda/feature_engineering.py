5a924b28c0dc8c8cff97f724b2f9d9029cde3ff7
Vinnicius Toth <vinni.toth@gmail.com>
2026-03-07 14:37:33 -0300

    infra: Adicionando app lambda

---
+++ app_services/lambda/feature_engineering.py
@@ -0,0 +1,48 @@
+import pandas as pd
+
+
+def preprocess(df):
+
+    # corrigir decimais
+    df = df.replace(",", ".", regex=True)
+
+    numeric_cols = [
+        "INDE 22",
+        "IAA",
+        "IEG",
+        "IPS",
+        "IDA",
+        "IPV",
+        "IAN",
+        "Matem",
+        "Portug",
+        "Inglês"
+    ]
+
+    for col in numeric_cols:
+        df[col] = pd.to_numeric(df[col], errors="coerce")
+
+    # criar target
+    df["RISCO_DEFASAGEM"] = (df["Defas"] > 0).astype(int)
+
+    # features finais
+    features = [
+        "Idade 22",
+        "INDE 22",
+        "IAA",
+        "IEG",
+        "IPS",
+        "IDA",
+        "IPV",
+        "IAN",
+        "Matem",
+        "Portug",
+        "Inglês",
+        "RISCO_DEFASAGEM"
+    ]
+
+    df = df[features]
+
+    df = df.fillna(df.median())
+
+    return df
--- app_services/lambda/handler.py
+++ app_services/lambda/handler.py
@@ -1,2 +1,37 @@
-def soma(a, b):
-    return a + b
+import pandas as pd
+import boto3
+from feature_engineering import preprocess
+
+s3 = boto3.client("s3")
+
+def lambda_handler(event, context):
+
+    bucket_raw = "mlops-pipeline-passosmagicos-prod-raw"
+    bucket_gold = "mlops-pipeline-passosmagicos-prod-gold"
+
+    files = [
+        "raw/alunos_2022.csv",
+        "raw/alunos_2023.csv",
+        "raw/alunos_2024.csv"
+    ]
+
+    dfs = []
+
+    for file in files:
+        obj = s3.get_object(Bucket=bucket_raw, Key=file)
+        df = pd.read_csv(obj["Body"], sep=";")
+        dfs.append(df)
+
+    df_all = pd.concat(dfs)
+
+    df_clean = preprocess(df_all)
+
+    df_clean.to_csv("/tmp/features.csv", index=False)
+
+    s3.upload_file(
+        "/tmp/features.csv",
+        bucket_gold,
+        "gold/features_dataset.csv"
+    )
+
+    return {"status": "ok"}    
