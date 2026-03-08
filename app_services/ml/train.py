af8431e7ae4a6996fdac2534f7a4169ab7c2bb4a
Vinnicius Toth <vinni.toth@gmail.com>
2026-03-08 14:45:15 -0300

    feat: Atualizando processamento ETL Lambda

--- app_services/ml/train.py
+++ app_services/ml/train.py
@@ -3,7 +3,6 @@ import joblib
 import boto3
 import numpy as np
 from datetime import datetime
-
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import (
